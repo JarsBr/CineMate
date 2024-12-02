38b80da90c4c3833cfc3cc8d2a3c46424c774b7a
Ítalo Faria <italogmfaria@gmail.com>
2024-12-02 09:48:40 -0300

    fix: alterando a navegação entre os fragmentos para do tipo binding.

---
+++ app/src/main/java/com/cinemate/app/ui/fragments/login/MainFragment.kt
@@ -0,0 +1,41 @@
+package com.cinemate.app.ui.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.cinemate.app.R
+import com.cinemate.app.databinding.FragmentMainBinding
+
+class MainFragment : Fragment() {
+
+    private var _binding: FragmentMainBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentMainBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnLogin.setOnClickListener {
+            findNavController().navigate(R.id.action_mainFragment_to_loginFragment)
+        }
+
+        binding.btnCadastro.setOnClickListener {
+            findNavController().navigate(R.id.action_mainFragment_to_cadastroFragment)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
